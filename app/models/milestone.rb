72fb13c756379e9a019bff97be9709fa08dfb2aa
Dennis Krupenik <dennis@krupenik.com>
2010-09-05 15:28:44 +0300

    typo fixes

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -4,14 +4,14 @@ class Milestone < ActiveRecord::Base
   def self.set(date = Date.today)
     prev_milestone = Milestone.first(
       :conditions => ['created_at <= ?', date],
-      :order => 'created_at desc', :limit => 1,
+      :order => 'created_at desc', :limit => 1
     )
     prev_date = prev_milestone.created_at + 1 rescue nil
     prev_amount = prev_milestone.amount rescue 0
     raise IntervalError, "too small" if (prev_date && 28 >= (date - prev_date))
     amounts = Entry.created_at(prev_date, date).scoped(:select => 'amount').map(&:amount)
     return Milestone.create(
-      :incomings => amounts.select{ |i| i > 0 }.sum + prev_amount),
+      :incomings => amounts.select{ |i| i > 0 }.sum + prev_amount,
       :expenses => amounts.select{ |i| i < 0 }.sum.abs,
       :created_at => date
     )
