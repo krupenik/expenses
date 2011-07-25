9264e4f2ea4b042a804bbfeffc71bbd8b4f5d006
Dennis Krupenik <dennis@krupenik.com>
2011-07-25 22:02:21 +0300

    removed published secret token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Expenses::Application.config.secret_token = 'FW5dpF8dyrZdZBYg6g3v69iIB4mcrB0YMzK3RajLo7dP1zejhPNBkXZKXwTYfo6A'
