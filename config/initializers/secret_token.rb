07859352eb1ff4dc876735e6162c43fbf0fc5d28
Dennis Krupenik <dennis@krupenik.com>
2011-07-25 21:39:57 +0300

    changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Expenses::Application.config.secret_token = '1dbccf000222d808c064fb3de0604f6540bafa0e34a253fd691f00d4c70e05d6584298cf0e1f006b05ecd82d4308f5d8012b383e2d2ded32273e85b73e7fc053'
+Expenses::Application.config.secret_token = 'FW5dpF8dyrZdZBYg6g3v69iIB4mcrB0YMzK3RajLo7dP1zejhPNBkXZKXwTYfo6A'
